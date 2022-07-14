53b557a87a635a263fc074e4d004d9a644b85806
Mike Mahoney <mike.mahoney.218@gmail.com>
2022-07-14 13:59:06 -0400

    Add GF paper

---
+++ papers/ground_filtering_manuscript/code/make_names.R
@@ -0,0 +1,19 @@
+coverage_names <- c(
+  "3 County" = "3County",
+  "Allegany & Steuben" = "AlleganySteuben",
+  "Cayuga & Oswego" = "CayugaOswego",
+  "Clinton, Essex & Franklin" = "ClintonEssexFranklin",
+  "Columbia & Rensselaer" = "ColumbiaRensselaer",
+  "Combined" = "Combined",
+  "Erie, Genesee & Livingston" = "ErieGeneseeLivingston",
+  "Franklin & St. Lawrence" = "FranklinStLawrence",
+  "Fulton, Saratoga, Herkimer & Franklin" = "FultonSaratogaHerkimerFranklin",
+  "Great Lakes" = "GreatLakes",
+  "Long Island" = "LongIsland",
+  "Madison & Otsego" = "MadisonOtsego",
+  "Oneida Subbasin" = "OneidaSubbasin",
+  "Schoharie" = "Schoharie",
+  "Southwest (spring)" = "Southwest",
+  "Southwest (fall)" = "SouthwestB",
+  "Warren, Washington & Essex" = "WarrenWashingtonEssex"
+)
